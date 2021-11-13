6bacb1284ae2132ea348b6b46721f54175d96783
DevHumbleChris <dev.humblechris254@gmail.com>
2021-11-13 11:55:29 +0300

    Saving changes to github

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,31 @@
 </template>
 
 <script>
+import { onBeforeMount } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import Navbar from '@/components/Navbar.vue'
+
 export default {
   components: {
     Navbar
+  },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    onBeforeMount(() => {
+      const auth = getAuth()
+
+      onAuthStateChanged(auth, (user) => {
+        console.log(user)
+        if (!user) {
+          router.replace('/login')
+        } else if (route.path === '/login' || route.path === '/signup') {
+          router.replace('/')
+        }
+      })
+    })
   }
 }
 </script>
